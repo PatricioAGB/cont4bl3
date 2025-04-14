291f22bcebe59aecad7e130647815630adbc61f1
PatricioAGB <Razer81368@gmail.com>
2025-04-14 16:53:00 -0400

    Intento de CICD

--- src/main/java/com/store/cont4bl3/controller/HomeController.java
+++ src/main/java/com/store/cont4bl3/controller/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
     public String home() {
         return "Hello home";
     }
-    @GetMapping("/hola1")
+    @GetMapping("/funciona")
     public String secured() {
         return "Funciona el CICD";
     }
