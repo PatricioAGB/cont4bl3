6e642054a802ef9b65d5ccaaf7cdf176872ab235
PatricioAGB <Razer81368@gmail.com>
2025-04-14 13:30:40 -0400

    Cambio endpoint, probando CICD otra vez

--- src/main/java/com/store/cont4bl3/controller/HomeController.java
+++ src/main/java/com/store/cont4bl3/controller/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
     public String home() {
         return "Hello home";
     }
-    @GetMapping("/hola1")
+    @GetMapping("/hola")
     public String secured() {
         return "Funciona el CICD";
     }
