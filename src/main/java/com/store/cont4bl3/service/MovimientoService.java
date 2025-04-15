42e8fcac37ca6811e776608eca9065b8636a9449
PatricioAGB <Razer81368@gmail.com>
2025-04-15 10:37:12 -0400

    Cambios al service de Movimientos, arreglo de ActualizarTablaMayor que no subia el Id de la cuenta

--- src/main/java/com/store/cont4bl3/service/MovimientoService.java
+++ src/main/java/com/store/cont4bl3/service/MovimientoService.java
@@ -1,7 +1,9 @@
 package com.store.cont4bl3.service;
 
+import com.store.cont4bl3.model.CuentaContableModel;
 import com.store.cont4bl3.model.LibroMayorModel;
 import com.store.cont4bl3.model.MovimientoModel;
+import com.store.cont4bl3.repository.CuentaContableRepository;
 import com.store.cont4bl3.repository.LibroMayorRepository;
 import com.store.cont4bl3.repository.MovimientoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,19 +21,25 @@ public class MovimientoService {
     @Autowired
     LibroMayorRepository libroMayorRepository;
 
+    @Autowired
+    CuentaContableRepository cuentaContableRepository;
+
     //Buscar todos lod movimientos
     public List<MovimientoModel> findAll() {
         return movimientoRepository.findAll();
     }
 
+    //Guardar Movimiento
     public MovimientoModel save(MovimientoModel movimientoModel) {
-        //Guardar Movimiento
         movimientoModel.setFechaMovimiento(LocalDate.now());
         MovimientoModel saved = movimientoRepository.save(movimientoModel);
 
+        CuentaContableModel cuentaDebe = cuentaContableRepository.findById(saved.getCuentaDebe().getIdCuenta()).orElseThrow();
+        CuentaContableModel cuentaHaber = cuentaContableRepository.findById(saved.getCuentaHaber().getIdCuenta()).orElseThrow();
+
         //Actualizar Libro mayor para la cuenta Debe
         actualizarLibroMayor(
-                saved.getCuentaDebe().getIdCuenta(),
+                cuentaDebe,
                 saved.getMonto(),
                 0, //haber
                 saved.getDescripcion()
@@ -39,7 +47,7 @@ public class MovimientoService {
 
         //Actualizar Libro mayor para la cuenta haber
         actualizarLibroMayor(
-          saved.getCuentaHaber().getIdCuenta(),
+          cuentaHaber,
           0, //debe
           saved.getMonto(),
           saved.getDescripcion()
@@ -47,20 +55,20 @@ public class MovimientoService {
         return saved;
     }
 
-    private void actualizarLibroMayor(Integer idCuenta, double debe,double haber, String descripcion) {
-    LibroMayorModel ultimoLibroMayor = libroMayorRepository.findTopByCuentaIdCuentaOrderByFechaDesc(idCuenta);
-    double saldoAnterior = (ultimoLibroMayor != null) ? ultimoLibroMayor.getSaldoActual():0;
-    double saldoActual = saldoAnterior + debe - haber ;
+    private void actualizarLibroMayor(CuentaContableModel cuenta, double debe, double haber, String descripcion) {
+        LibroMayorModel ultimoLibroMayor = libroMayorRepository.findTopByCuentaIdCuentaOrderByFechaDesc(cuenta.getIdCuenta());
+        double saldoAnterior = (ultimoLibroMayor != null) ? ultimoLibroMayor.getSaldoActual() : cuenta.getSaldoInicial();
+        double saldoActual = saldoAnterior + debe - haber;
 
-    LibroMayorModel nuevoRegistro = new LibroMayorModel();
-    nuevoRegistro.setCuenta(ultimoLibroMayor != null ? ultimoLibroMayor.getCuenta() : null);
-    nuevoRegistro.setFecha(LocalDate.now());
-    nuevoRegistro.setSaldoAnterior(saldoAnterior);
-    nuevoRegistro.setDebe(debe);
-    nuevoRegistro.setHaber(haber);
-    nuevoRegistro.setSaldoActual(saldoActual);
+        LibroMayorModel nuevoRegistro = new LibroMayorModel();
+        nuevoRegistro.setCuenta(cuenta);
+        nuevoRegistro.setFecha(LocalDate.now());
+        nuevoRegistro.setSaldoAnterior(saldoAnterior);
+        nuevoRegistro.setDebe(debe);
+        nuevoRegistro.setHaber(haber);
+        nuevoRegistro.setSaldoActual(saldoActual);
 
-    libroMayorRepository.save(nuevoRegistro);
-
+        libroMayorRepository.save(nuevoRegistro);
     }
+
 }
