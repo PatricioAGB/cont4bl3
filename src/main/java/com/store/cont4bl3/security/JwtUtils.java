c86cebd739a5c69a7ee71057f0e9afcf8f695f71
PatricioAGB <Razer81368@gmail.com>
2025-04-22 10:55:59 -0400

    cambios en la tabla y obtencion de jwt del token

--- src/main/java/com/store/cont4bl3/security/JwtUtils.java
+++ src/main/java/com/store/cont4bl3/security/JwtUtils.java
@@ -7,13 +7,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtUtils {
 
-    private final String SECRET_KEY = "clave_secreta"; // usa la misma de auth-service
+    private final String SECRET_KEY = "ProyectoCont4bl3ACL"; // usa la misma que en auth-service
 
-    public String extractUsername(String token) {
+    public Claims extractClaims(String token) {
         return Jwts.parser()
                 .setSigningKey(SECRET_KEY.getBytes())
                 .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+                .getBody();
+    }
+
+    public String extractUsername(String token) {
+        Claims claims = extractClaims(token);
+
+        // Primero intenta leer el `sub`, como debe ser
+        String username = claims.getSubject();
+
+        // Si no está, intenta con el claim explícito `usuario`
+        if (username == null && claims.get("usuario") != null) {
+            username = claims.get("usuario", String.class);
+        }
+
+        return username;
     }
 }
