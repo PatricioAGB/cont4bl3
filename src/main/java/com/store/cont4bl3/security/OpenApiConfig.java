2ab23bbb5452660b758ddb2327741bc576e565c7
PatricioAGB <Razer81368@gmail.com>
2025-04-22 19:09:04 -0400

    se agregaron dtos y se cambiaron los servicios

---
+++ src/main/java/com/store/cont4bl3/security/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.store.cont4bl3.security;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API Cont4bl3").version("1.0").description("Documentación de API Cont4bl3"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth", List.of()));
+    }
+}
